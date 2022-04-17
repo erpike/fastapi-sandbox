8faeba8cc666e556f2de7091030f56a5e840816e
Roman Schuka <pazitivnyi@gmail.com>
2022-04-17 19:18:42 +0300

    bash scripts modified

---
+++ clean-docker.sh
@@ -0,0 +1,3 @@
+# uvicorn src.main:app --reload
+sudo docker rm erp-fastapi-dev --force
+sudo docker rmi erp-fastapi
--- run.sh
+++ run.sh
@@ -3,6 +3,3 @@
 sudo docker build -t erp-fastapi .
 sudo docker run -d --name erp-fastapi-dev -p 80:80 erp-fastapi
 sudo docker logs -f erp-fastapi-dev
-
-sudo docker rm erp-fastapi-dev --force
-sudo docker rmi erp-fastapi
